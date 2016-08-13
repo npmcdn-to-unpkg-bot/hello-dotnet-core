acf5a6d12d0eaa9c54cc558c6f61632706dbef5b
jeffhollan <jeffhollan@outlook.com>
2016-08-13 15:49:46 -0700

    initial

---
+++ Assets/Scripts/main.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/hello";
+
+ReactDOM.render(
+    <Hello compiler="Typescript" framework="React" />,
+    document.getElementById("example")
+);
